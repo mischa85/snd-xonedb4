35dea3dd64db3c2e55533d81e7ee58b088d5b0e6
Marcel Bierling <marcel@hackerman.art>
2025-01-04 01:22:11 +0100

    linux: big code cleanup, rewrote for interrupt urbs, Xone:DB2 added, MIDI OUT

--- common/ploytec.h
+++ common/ploytec.h
@@ -3,5 +3,4 @@
 
 void ploytec_convert_from_s24_3le(uint8_t *dest, uint8_t *src);
 void ploytec_convert_to_s24_3le(uint8_t *dest, uint8_t *src);
-void ploytec_sync_bytes(uint8_t *dest);
 #endif /* PLOYTEC_H */
--- linux-alsa/midi.h
+++ linux-alsa/midi.h
@@ -4,5 +4,7 @@
 struct xonedb4_chip;
 
 int xonedb4_midi_init(struct xonedb4_chip *chip);
+int xonedb4_midi_init_bulk_urbs(struct xonedb4_chip *chip);
+void xonedb4_get_midi_output(u8 *buffer, int count);
 void xonedb4_midi_abort(struct xonedb4_chip *chip);
 #endif /* XONEDB4_MIDI_H */
